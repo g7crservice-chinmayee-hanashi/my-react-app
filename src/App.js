179c0b5160bde919fd8306bd84d30c0b084334a1
g7crservice-chinmayee-hanashi <120355187+g7crservice-chinmayee-hanashi@users.noreply.github.com>
2025-08-24 11:00:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Good Morning! {process.evn.NAME}</div>;
+  return <div className="App">Hello Good Morning! {process.evn.REACT_APP_NAME}</div>;
 }
 
 export default App;
